db66fc174a03c609d3ecc2a715bbc8b0b9773382
Spacegirl2525 <54623172+Spacegirl2525-bot@users.noreply.github.com>
2021-03-03 15:56:54 +1300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿
+namespace Project_Charlie_Elize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4Email = new System.Windows.Forms.TextBox();
+            this.textBox3Mobile = new System.Windows.Forms.TextBox();
+            this.textBox2Surname = new System.Windows.Forms.TextBox();
+            this.textBox1Name = new System.Windows.Forms.TextBox();
+            this.label7Email = new System.Windows.Forms.Label();
+            this.label6Mobile = new System.Windows.Forms.Label();
+            this.label5Surname = new System.Windows.Forms.Label();
+            this.label4Name = new System.Windows.Forms.Label();
+            this.label1VisDeets = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label11Meetingaim = new System.Windows.Forms.Label();
+            this.label10Meetingwith = new System.Windows.Forms.Label();
+            this.label9Time = new System.Windows.Forms.Label();
+            this.label8Date = new System.Windows.Forms.Label();
+            this.label2MeetingDeets = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4Email);
+            this.groupBox1.Controls.Add(this.textBox3Mobile);
+            this.groupBox1.Controls.Add(this.textBox2Surname);
+            this.groupBox1.Controls.Add(this.textBox1Name);
+            this.groupBox1.Controls.Add(this.label7Email);
+            this.groupBox1.Controls.Add(this.label6Mobile);
+            this.groupBox1.Controls.Add(this.label5Surname);
+            this.groupBox1.Controls.Add(this.label4Name);
+            this.groupBox1.Controls.Add(this.label1VisDeets);
+            this.groupBox1.Location = new System.Drawing.Point(25, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 298);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox4Email
+            // 
+            this.textBox4Email.Location = new System.Drawing.Point(15, 232);
+            this.textBox4Email.Name = "textBox4Email";
+            this.textBox4Email.Size = new System.Drawing.Size(209, 20);
+            this.textBox4Email.TabIndex = 8;
+            // 
+            // textBox3Mobile
+            // 
+            this.textBox3Mobile.Location = new System.Drawing.Point(15, 183);
+            this.textBox3Mobile.Name = "textBox3Mobile";
+            this.textBox3Mobile.Size = new System.Drawing.Size(209, 20);
+            this.textBox3Mobile.TabIndex = 7;
+            // 
+            // textBox2Surname
+            // 
+            this.textBox2Surname.Location = new System.Drawing.Point(15, 133);
+            this.textBox2Surname.Name = "textBox2Surname";
+            this.textBox2Surname.Size = new System.Drawing.Size(209, 20);
+            this.textBox2Surname.TabIndex = 6;
+            // 
+            // textBox1Name
+            // 
+            this.textBox1Name.Location = new System.Drawing.Point(15, 80);
+            this.textBox1Name.Name = "textBox1Name";
+            this.textBox1Name.Size = new System.Drawing.Size(209, 20);
+            this.textBox1Name.TabIndex = 5;
+            this.textBox1Name.TextChanged += new System.EventHandler(this.textBox1Name_TextChanged);
+            // 
+            // label7Email
+            // 
+            this.label7Email.AutoSize = true;
+            this.label7Email.Location = new System.Drawing.Point(14, 211);
+            this.label7Email.Name = "label7Email";
+            this.label7Email.Size = new System.Drawing.Size(32, 13);
+            this.label7Email.TabIndex = 4;
+            this.label7Email.Text = "Email";
+            // 
+            // label6Mobile
+            // 
+            this.label6Mobile.AutoSize = true;
+            this.label6Mobile.Location = new System.Drawing.Point(14, 163);
+            this.label6Mobile.Name = "label6Mobile";
+            this.label6Mobile.Size = new System.Drawing.Size(38, 13);
+            this.label6Mobile.TabIndex = 3;
+            this.label6Mobile.Text = "Mobile";
+            // 
+            // label5Surname
+            // 
+            this.label5Surname.AutoSize = true;
+            this.label5Surname.Location = new System.Drawing.Point(14, 114);
+            this.label5Surname.Name = "label5Surname";
+            this.label5Surname.Size = new System.Drawing.Size(49, 13);
+            this.label5Surname.TabIndex = 2;
+            this.label5Surname.Text = "Surname";
+            // 
+            // label4Name
+            // 
+            this.label4Name.AutoSize = true;
+            this.label4Name.Location = new System.Drawing.Point(14, 60);
+            this.label4Name.Name = "label4Name";
+            this.label4Name.Size = new System.Drawing.Size(35, 13);
+            this.label4Name.TabIndex = 1;
+            this.label4Name.Text = "Name";
+            // 
+            // label1VisDeets
+            // 
+            this.label1VisDeets.AutoSize = true;
+            this.label1VisDeets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1VisDeets.Location = new System.Drawing.Point(12, 25);
+            this.label1VisDeets.Name = "label1VisDeets";
+            this.label1VisDeets.Size = new System.Drawing.Size(105, 16);
+            this.label1VisDeets.TabIndex = 0;
+            this.label1VisDeets.Text = "Visitor Details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label11Meetingaim);
+            this.groupBox2.Controls.Add(this.label10Meetingwith);
+            this.groupBox2.Controls.Add(this.label9Time);
+            this.groupBox2.Controls.Add(this.label8Date);
+            this.groupBox2.Controls.Add(this.label2MeetingDeets);
+            this.groupBox2.Location = new System.Drawing.Point(293, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(242, 296);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = ":";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 26);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Site Visit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Joe Dart",
+            "Cory Wong",
+            "Jack Stratton",
+            "Woody Goss",
+            "Theo Katzman",
+            "Antwaun Stanley"});
+            this.comboBox2.Location = new System.Drawing.Point(3, 194);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(204, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(71, 132);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 133);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 77);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(204, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label11Meetingaim
+            // 
+            this.label11Meetingaim.AutoSize = true;
+            this.label11Meetingaim.Location = new System.Drawing.Point(7, 232);
+            this.label11Meetingaim.Name = "label11Meetingaim";
+            this.label11Meetingaim.Size = new System.Drawing.Size(65, 13);
+            this.label11Meetingaim.TabIndex = 4;
+            this.label11Meetingaim.Text = "Meeting Aim";
+            // 
+            // label10Meetingwith
+            // 
+            this.label10Meetingwith.AutoSize = true;
+            this.label10Meetingwith.Location = new System.Drawing.Point(0, 173);
+            this.label10Meetingwith.Name = "label10Meetingwith";
+            this.label10Meetingwith.Size = new System.Drawing.Size(70, 13);
+            this.label10Meetingwith.TabIndex = 3;
+            this.label10Meetingwith.Text = "Meeting With";
+            // 
+            // label9Time
+            // 
+            this.label9Time.AutoSize = true;
+            this.label9Time.Location = new System.Drawing.Point(6, 112);
+            this.label9Time.Name = "label9Time";
+            this.label9Time.Size = new System.Drawing.Size(30, 13);
+            this.label9Time.TabIndex = 2;
+            this.label9Time.Text = "Time";
+            // 
+            // label8Date
+            // 
+            this.label8Date.AutoSize = true;
+            this.label8Date.Location = new System.Drawing.Point(6, 58);
+            this.label8Date.Name = "label8Date";
+            this.label8Date.Size = new System.Drawing.Size(30, 13);
+            this.label8Date.TabIndex = 1;
+            this.label8Date.Text = "Date";
+            // 
+            // label2MeetingDeets
+            // 
+            this.label2MeetingDeets.AutoSize = true;
+            this.label2MeetingDeets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2MeetingDeets.Location = new System.Drawing.Point(0, 23);
+            this.label2MeetingDeets.Name = "label2MeetingDeets";
+            this.label2MeetingDeets.Size = new System.Drawing.Size(116, 16);
+            this.label2MeetingDeets.TabIndex = 0;
+            this.label2MeetingDeets.Text = "Meeting Details";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(566, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(277, 338);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Visitors Onsite";
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignInButton.Location = new System.Drawing.Point(40, 353);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(444, 58);
+            this.SignInButton.TabIndex = 3;
+            this.SignInButton.Text = "Sign In";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 424);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4Email;
+        private System.Windows.Forms.TextBox textBox3Mobile;
+        private System.Windows.Forms.TextBox textBox2Surname;
+        private System.Windows.Forms.TextBox textBox1Name;
+        private System.Windows.Forms.Label label7Email;
+        private System.Windows.Forms.Label label6Mobile;
+        private System.Windows.Forms.Label label5Surname;
+        private System.Windows.Forms.Label label4Name;
+        private System.Windows.Forms.Label label1VisDeets;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label11Meetingaim;
+        private System.Windows.Forms.Label label10Meetingwith;
+        private System.Windows.Forms.Label label9Time;
+        private System.Windows.Forms.Label label8Date;
+        private System.Windows.Forms.Label label2MeetingDeets;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SignInButton;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Charlie_Elize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            double Mobile;
+            string Name;
+            string Email;
+            string Surname;
+            string Date;
+            string Time1;
+            string Time2;
+            string Time;
+            string Meeting;
+
+            Name = textBox1Name.Text;
+            Surname = textBox2Surname.Text;
+            Mobile = double.Parse(textBox3Mobile.Text);
+            Email = textBox4Email.Text;
+            Date = dateTimePicker1.Text;
+            Time1 = Convert.ToString(numericUpDown1);
+            Time2 = Convert.ToString(numericUpDown2);
+            Time = Convert.ToInt32 (Time1, Time2);
+            Meeting = comboBox2.Text;
+            
+
+            listBox1.Items.Add("----- New Sign In ----");
+            listBox1.Items.Add(Name);
+            listBox1.Items.Add(Surname);
+            listBox1.Items.Add(Mobile);
+            listBox1.Items.Add(Email);
+            listBox1.Items.Add(Date);
+            listBox1.Items.Add(Time);
+            listBox1.Items.Add(Meeting);
+            listBox1.Items.Add("-------------------------");
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 F2 = new Form2();
+            F2.ShowDialog();
+
+            if (F2.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void textBox1Name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
